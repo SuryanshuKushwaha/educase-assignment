1d096c12ae8bc8e886fa8b70a9497fcdc6b95d68
Suryanshu Kushwaha <114632428+SuryanshuKushwaha@users.noreply.github.com>
2025-09-02 13:39:27 +0530

    Delete educase-assignment-master/educase-assignment-master directory

--- educase-assignment-master/educase-assignment-master/src/components/ui/Paragraph.tsx
+++
@@ -1,3 +0,0 @@
-export const Paragraph = ({ content }: { content: string }) => {
-  return <p className="text-[16px] text-neutral-500 ">{content}</p>;
-};
--- educase-assignment-master/educase-assignment-master/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import Router from "./router.tsx";
-
-createRoot(document.getElementById("root")!).render(<Router />);
